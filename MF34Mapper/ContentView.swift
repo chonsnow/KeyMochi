e82242e14df2c33d8261074fc7bd0a6c046c5a08
chonsnow <octab@naver.com>
2026-09-24 00:11:07 +0900

    Initial Commit

---
+++ MF34Mapper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MF34Mapper
+//
+//  Created by leepeel on 9/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MF34Mapper/MF34MapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MF34MapperApp.swift
+//  MF34Mapper
+//
+//  Created by leepeel on 9/24/26.
+//
+
+import SwiftUI
+
+@main
+struct MF34MapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
